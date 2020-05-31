39602f41e3ae09d90e04e2933ed5c583533feeb0
Luke Gardener <lgardener1337@gmail.com>
2020-05-31 16:17:46 +0900

    First commit

---
+++ src/main/java/com/arkumbra/gameboykooler/AddressBus.java
@@ -0,0 +1,9 @@
+package com.arkumbra.gameboykooler;
+
+/**
+ * The Game Boy (Color) uses memory mapping to allow access to all of its hardware. This address
+ * bus handles that mapping, passing the
+ */
+public class AddressBus {
+
+}
